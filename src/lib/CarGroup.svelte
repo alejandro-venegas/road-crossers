b423844dd0c75e3dfdc1160c199199791a82ebe6
Alejandro Venegas <avenegasc14@gmail.com>
2022-04-28 13:02:06 -0600

    Add car

---
+++ src/lib/CarGroup.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import * as THREE from 'three';
+    import * as SC from 'svelte-cubed';
+    const vehicleColors = [0xa52523, 0xbdb638, 0x78b141];
+</script>
+
+<SC.Group>
+    <SC.Mesh  geometry={new THREE.BoxBufferGeometry(10, 10, 36)}
+              material={new THREE.MeshLambertMaterial({color: 0x333333 })}
+              position={[-18, 5, 0]}
+    />
+    <SC.Mesh  geometry={new THREE.BoxBufferGeometry(10, 10, 36)}
+              material={new THREE.MeshLambertMaterial({color: 0x333333 })}
+              position={[18, 5, 0]}
+    />
+    <SC.Mesh  geometry={new THREE.BoxBufferGeometry(70, 10, 30)}
+              material={new THREE.MeshLambertMaterial({color: vehicleColors[0] })}
+              position={[0, 10, 0]}
+    />
+    <SC.Mesh  geometry={new THREE.BoxBufferGeometry(40, 10, 25)}
+              material={new THREE.MeshLambertMaterial({color: 0xFFFFFFF })}
+              position={[-5, 20, 0]}
+    />
+
+</SC.Group>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,60 +1,11 @@
-<script context="module" lang="ts">
-	export const prerender = true;
+<script>
+	import * as SC from 'svelte-cubed';
+	import CarGroup from '$lib/CarGroup.svelte';
 </script>
 
-<script lang="ts">
-	import Counter from '$lib/Counter.svelte';
-</script>
-
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<SC.Canvas>
+    <CarGroup/>
+	<SC.AmbientLight color={0xFFFFFF} intensity="0.6" />
+	<SC.DirectionalLight color={0xFFFFFF} intensity="0.6" position={[20, 30, 100]}/>
+	<SC.PerspectiveCamera position={[100, 150, 150]} />
+</SC.Canvas>
