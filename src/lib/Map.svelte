9d783703b994f9ca7a9bb423b492bd5fb6e0a0aa
Alejandro Venegas <avenegasc14@gmail.com>
2022-04-28 13:53:20 -0600

    Add map floor

---
+++ src/lib/Map.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import * as THREE from 'three';
+    import * as SC from 'svelte-cubed';
+
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+
+
+</script>
+
+<SC.Group>
+    <SC.Mesh geometry={new THREE.PlaneBufferGeometry(width, height)}
+             material={new THREE.MeshLambertMaterial({color: 0x00cc00})}
+             rotation={[-90 * Math.PI / 180, 0, 0]}
+
+    />
+</SC.Group>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,14 @@
-<script>
+<script lang="ts">
 	import * as SC from 'svelte-cubed';
+
 	import Car from '$lib/Car.svelte';
+	import Map from '$lib/Map.svelte';
+
+	let canvas;
 </script>
 
-<SC.Canvas>
+<SC.Canvas antialias bind:this={canvas}>
+	<Map/>
     <Car/>
 	<SC.AmbientLight color={0xFFFFFF} intensity="0.6" />
 	<SC.DirectionalLight color={0xFFFFFF} intensity="0.6" position={[20, 30, 100]}/>
