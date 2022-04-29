a0d1c97a87a64fd999aac9b43694476232ae6133
Alejandro Venegas <avenegasc14@gmail.com>
2022-04-29 17:39:37 -0600

    Add hit detection

--- src/lib/Car.svelte
+++ src/lib/Car.svelte
@@ -3,13 +3,30 @@
 	import * as SC from 'svelte-cubed';
 	import { createEventDispatcher } from 'svelte';
 
+	import { position, width } from '$lib/Player.ts';
+	import { isOver } from './Game';
+
 	export let cameraWidth;
 	export let velocity;
+	export let globalZ;
+
+	let car;
 
 	const dispatch = createEventDispatcher();
 
-	const vehicleColors = [0xeb4034, 0xebdc34, 0x5ceb34, 0x34dfeb, 0xe534eb, 0xeb7734];
+	const vehicleColors = [
+		0xeb4034, 0xebdc34, 0x5ceb34, 0x34dfeb, 0xe534eb, 0xeb7734
+	];
 	const colorIndex = Math.floor(Math.random() * vehicleColors.length);
+
+	function aabb(a, b) {
+		return (
+			a.x < b.x + b.width &&
+			a.x + a.width > b.x &&
+			a.z < b.z + b.depth &&
+			a.z + a.depth > b.z
+		);
+	}
 
 	let xOffset = 0;
 
@@ -17,16 +34,32 @@
 	let x;
 
 	SC.onFrame(() => {
+		if ($isOver) {
+			return;
+		}
 		if (xInit >= -x) {
 			dispatch('remove');
 			return;
 		}
 		xOffset += 1;
 		x = xInit + xOffset * velocity;
+
+		const playerInfo = {
+			x: $position.x - $width / 2,
+			z: $position.z - $width / 2,
+			width: $width,
+			depth: $width
+		};
+
+		const carInfo = { x: x - 35, z: globalZ - 15, width: 70, depth: 30 };
+
+		if (aabb(playerInfo, carInfo)) {
+			$isOver = true;
+		}
 	});
 </script>
 
-<SC.Group position={[x, 0, 0]}>
+<SC.Group position={[x, 0, 0]} bind:this={car}>
 	<SC.Mesh
 		geometry={new THREE.BoxBufferGeometry(10, 10, 36)}
 		material={new THREE.MeshStandardMaterial({ color: 0x333333 })}
@@ -43,7 +76,9 @@
 	/>
 	<SC.Mesh
 		geometry={new THREE.BoxBufferGeometry(70, 15, 30)}
-		material={new THREE.MeshStandardMaterial({ color: vehicleColors[colorIndex] })}
+		material={new THREE.MeshStandardMaterial({
+			color: vehicleColors[colorIndex]
+		})}
 		position={[0, 10, 0]}
 		castShadow
 		receiveShadow
